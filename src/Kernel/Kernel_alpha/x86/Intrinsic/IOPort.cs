eff6cbf137a10b6d4389dd682763ee2856f8b2be
Aman Priyadarshi <aman.eureka@gmail.com>
2014-07-06 19:57:21 +0530

    Added IOPorts !! WOOP :))

---
+++ src/Kernel/Kernel_alpha/x86/Intrinsic/IOPort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel_alpha.x86.Intrinsic
+{
+    // Awesome IOPorts :)
+    public class IOPort
+    {
+        public ushort Address;
+
+        public IOPort (ushort addr)
+        {
+            Address = addr;
+        }
+
+        public byte Byte
+        {
+            get
+            {
+                return Native.In8 (Address);
+            }
+            set
+            {
+                Native.Out8 (Address, value);
+            }
+        }
+
+        public ushort Word
+        {
+            get
+            {
+                return Native.In16 (Address);
+            }
+            set
+            {
+                Native.Out16 (Address, value);
+            }
+        }
+
+        public uint DWord
+        {
+            get
+            {
+                return Native.In32 (Address);
+            }
+            set
+            {
+                Native.Out32 (Address, value);
+            }
+        }
+    }
+}
