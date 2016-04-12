ee4a9bad3fa5f1bd920f91fb2ad8f4a1945ab618
Aman Priyadarshi <aman.eureka@gmail.com>
2016-04-12 05:53:42 +0530

    - Fixed target .NET Framework version - Added Fully Qualified Build.sh

---
+++ src/Build/Build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#  █████╗ ████████╗ ██████╗ ███╗   ███╗██╗██╗  ██╗     ██████╗ ███████╗
+# ██╔══██╗╚══██╔══╝██╔═══██╗████╗ ████║██║╚██╗██╔╝    ██╔═══██╗██╔════╝
+# ███████║   ██║   ██║   ██║██╔████╔██║██║ ╚███╔╝     ██║   ██║███████╗
+# ██╔══██║   ██║   ██║   ██║██║╚██╔╝██║██║ ██╔██╗     ██║   ██║╚════██║
+# ██║  ██║   ██║   ╚██████╔╝██║ ╚═╝ ██║██║██╔╝ ██╗    ╚██████╔╝███████║
+# ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═╝     ╚═════╝ ╚══════╝
+
+BUILD_CPU=x86
+BUILD_NAME=AtomOS-Alfa
+ATOMIX_APPS=Bin/Apps
+OUTPUT_DIR=Output
+ATOMIX_COMPILER_FLAGS="-d -optimize"
+ATOMIX_COMPILER=Bin/Atomixilc.exe
+ATOMIX_KERNEL=Bin/Atomix.Kernel_H.dll
+ATOMIX_ISO_DIR=ISO
+ATOMIX_RD=ramdisk
+ATOMIX_LIB=Bin/Atomix.Core.dll
+ATOMIX_RamFS=Bin/Atomix.RamFS.exe
+
+#BUILD KERNEL FIRST
+mono $ATOMIX_COMPILER -cpu $BUILD_CPU -i $ATOMIX_KERNEL -o $OUTPUT_DIR/Kernel.asm $ATOMIX_COMPILER_FLAGS
+nasm -fbin $OUTPUT_DIR/Kernel.asm -o $ATOMIX_ISO_DIR/Kernel.bin
+
+#BUILD APP ONE BY ONE
+for file in "$ATOMIX_APPS/*.dll"
+do
+    FILE_NAME="$(basename $file .dll)"
+    echo "[APP] $FILE_NAME"
+    mono $ATOMIX_COMPILER -cpu $BUILD_CPU -i "$ATOMIX_LIB;$ATOMIX_APPS/$FILE_NAME.dll" -o "$OUTPUT_DIR/Apps/$FILE_NAME.asm" $ATOMIX_COMPILER_FLAGS
+    nasm -felf "$OUTPUT_DIR/Apps/$FILE_NAME.asm" -o "$ATOMIX_RD/Apps/$FILE_NAME.bin"
+done
+
+#CREATE RAM DISK
+mono $ATOMIX_RamFS $ATOMIX_RD -o $ATOMIX_ISO_DIR/Initrd.bin
+
+#CREATE ISO IMAGE
+mkisofs -o $OUTPUT_DIR/AtomOS-Alfa.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table $ATOMIX_ISO_DIR
