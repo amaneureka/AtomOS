6a2b077bcef755fa49a5f424bcfbd6720b91208c
Aman Priyadarshi <aman.eureka@outlook.com>
2016-07-02 10:29:43 +0530

    Implemented Shr and Shr for xSize > 4 (<= 8)

--- src/Compiler/IL/Shr.cs
+++ src/Compiler/IL/Shr.cs
@@ -7,7 +7,6 @@
 * PROGRAMMERS:      Aman Priyadarshi (aman.eureka@gmail.com)
 */
 
-using System;
 using System.Reflection;
 
 using Atomix.Assembler;
@@ -21,13 +20,14 @@ namespace Atomix.IL
     public class ILShr : MSIL
     {
         public ILShr(Compiler Cmp)
-            : base("shr", Cmp) { }
+            : base("shr", Cmp)
+        { }
 
         public override void Execute(ILOpCode instr, MethodBase aMethod)
         {
             var xStackItem_ShiftAmount = Core.vStack.Pop();
             var xStackItem_Value = Core.vStack.Peek();
-            
+
             switch (ILCompiler.CPUArchitecture)
             {
                 #region _x86_
@@ -35,11 +35,38 @@ namespace Atomix.IL
                     {
                         if (xStackItem_Value.Size > 4)
                         {
-                            throw new Exception("Not Yet implemented");
+                            // stack item 1 - arg (x)
+                            // stack item 2 - shift amount (y)
+
+                            // x.Low = (x.Low >> y) | (x.High & ((1 << y) - 1)) << ((1 << (32 - y)) - 1)
+                            // x.High = (x.High >> y)
+                            Core.AssemblerCode.Add(new Pop { DestinationReg = Registers.ECX });
+
+                            // high
+                            Core.AssemblerCode.Add(new Mov { DestinationReg = Registers.EAX, SourceReg = Registers.ESP, SourceDisplacement = 4, SourceIndirect = true });
+
+                            Core.AssemblerCode.Add(new Cmp { DestinationReg = Registers.ECX, SourceRef = "0x20" });
+                            Core.AssemblerCode.Add(new Jmp { Condition = ConditionalJumpEnum.JL, DestinationRef = ".LessThan32" });
+                            // high
+                            Core.AssemblerCode.Add(new Mov { DestinationReg = Registers.ESP, DestinationDisplacement = 4, DestinationIndirect = true, SourceRef = "0x0" });
+                            // low
+                            Core.AssemblerCode.Add(new And { DestinationReg = Registers.ECX, SourceRef = "0x1F" });
+                            Core.AssemblerCode.Add(new ShiftRight { DestinationReg = Registers.EAX, SourceReg = Registers.CL });
+                            Core.AssemblerCode.Add(new Mov { DestinationReg = Registers.ESP, DestinationIndirect = true, DestinationDisplacement = 4, SourceReg = Registers.EAX });
+                            Core.AssemblerCode.Add(new Jmp { DestinationRef = ".ShrComplete" });
+
+                            Core.AssemblerCode.Add(new Label(".LessThan32"));
+                            // low
+                            Core.AssemblerCode.Add(new Mov { DestinationReg = Registers.EDX, SourceReg = Registers.ESP, SourceIndirect = true });
+                            // shift high
+                            Core.AssemblerCode.Add(new ShiftRight { DestinationReg = Registers.ESP, DestinationIndirect = true, DestinationDisplacement = 4, SourceReg = Registers.CL });
+                            // shift low
+                            Core.AssemblerCode.Add(new Literal("shrd dword EDX, EAX, CL"));
+                            Core.AssemblerCode.Add(new Label(".ShrComplete"));
                         }
                         else
                         {
-                            Core.AssemblerCode.Add(new Pop { DestinationReg = Registers.ECX }); //Shift Amount
+                            Core.AssemblerCode.Add(new Pop { DestinationReg = Registers.ECX }); // Shift Amount
                             Core.AssemblerCode.Add(new ShiftRight { DestinationReg = Registers.ESP, DestinationIndirect = true, SourceReg = Registers.CL });
                         }
                     }
@@ -48,17 +75,17 @@ namespace Atomix.IL
                 #region _x64_
                 case CPUArch.x64:
                     {
-                        
+
                     }
                     break;
                 #endregion
                 #region _ARM_
                 case CPUArch.ARM:
                     {
-                        
+
                     }
                     break;
-                #endregion
+                    #endregion
             }
         }
     }
