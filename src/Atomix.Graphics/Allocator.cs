c9d071852ade51b8ac2c117cda7ad52d8d659878
Splitty <splittydev@gmail.com>
2016-07-03 05:13:43 +0200

    Added Atomix.Graphics library
    - Started working on the Bitmap class

---
+++ src/Atomix.Graphics/Allocator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Atomix.Graphics
+{
+    public static class Allocator
+    {
+
+        /// <summary>
+        /// Alloc delegate.
+        /// </summary>
+        public unsafe delegate byte* AllocationDelegate(int size);
+
+        /// <summary>
+        /// The allocator.
+        /// </summary>
+        public static AllocationDelegate Alloc;
+
+        /// <summary>
+        /// Initializes the <see cref="T:Atomix.Graphics.Allocator"/> class.
+        /// </summary>
+        static Allocator()
+        {
+
+            // Set default allocator
+            Alloc = size =>
+            {
+                throw new Exception("Please set Allocator.Alloc!");
+            };
+        }
+    }
+}
+
+
---
+++ src/Atomix.Graphics/Bitmap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atomix.Graphics
+{
+
+    /// <summary>
+    /// Bitmap.
+    /// </summary>
+    public unsafe class Bitmap
+    {
+
+        /// <summary>
+        /// The pixels.
+        /// </summary>
+        readonly int* pixels;
+
+        /// <summary>
+        /// The width.
+        /// </summary>
+        public readonly int Width;
+
+        /// <summary>
+        /// The height.
+        /// </summary>
+        public readonly int Height;
+
+        /// <summary>
+        /// Gets the size of the bitmap in bytes.
+        /// </summary>
+        /// <value>The size.</value>
+        public int Size => Width * Height * 4;
+
+        public Bitmap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            pixels = (int *) Allocator.Alloc(width * height * 4);
+        }
+
+        public void SetPixel(int x, int y, byte r, byte g, byte b, byte a)
+        {
+            ClampCoords(&x, &y);
+            int rgba = a << 32 | b << 16 | g << 8 | r;
+            *(pixels + (y * Width + x)) = rgba;
+        }
+
+        void ClampCoords(int *x, int *y)
+        {
+            ClampPtr(x, 0, Width);
+            ClampPtr(y, 0, Height);
+        }
+
+        void ClampPtr(int *x, int min, int max)
+        {
+            *x = *x < min ? min : *x > max ? max : *x;
+        }
+    }
+}
+
