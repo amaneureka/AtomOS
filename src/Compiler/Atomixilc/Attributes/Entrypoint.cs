73319ddfb70108358280bb1ef9c70f4a2eb42ce6
Aman Priyadarshi <aman.eureka@outlook.com>
2016-11-27 06:40:38 +0530

    New Compiler - Basic Scanning and interface ready.

---
+++ src/Compiler/Atomixilc/Attributes/Entrypoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atomixilc.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class EntrypointAttribute : Attribute
+    {
+        public readonly Architecture Platform;
+
+        public EntrypointAttribute(Architecture arch)
+        {
+            Platform = arch;
+        }
+    }
+}
