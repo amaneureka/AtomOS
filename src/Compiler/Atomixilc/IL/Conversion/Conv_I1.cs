11a7fcb01c1e6b08d8bf70df4efb855049737039
Aman Priyadarshi <aman.eureka@outlook.com>
2016-12-02 18:25:33 +0530

    Added Conversion IL and Movzx, Movsx x86 Instructions

---
+++ src/Compiler/Atomixilc/IL/Conversion/Conv_I1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.IL.CodeType;
+using Atomixilc.Machine.x86;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Conv_I1)]
+    internal class Conv_I1_il : MSIL
+    {
+        internal Conv_I1_il()
+            : base(ILCode.Conv_I1)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Conv_I1(v=vs.110).aspx
+         * Description : Converts the value on top of the evaluation stack to native int
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 2)
+                throw new Exception("Internal Compiler Error: vStack.Count < 2");
+
+            var item = Optimizer.vStack.Pop();
+            var size = Helper.GetTypeSize(item.OperandType, Config.TargetPlatform);
+
+            /* The stack transitional behavior, in sequential order, is:
+             * value is pushed onto the stack.
+             * value is popped from the stack and the conversion operation is attempted.
+             * If the conversion is successful, the resulting value is pushed onto the stack.
+             */
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        if (item.IsFloat || size > 4)
+                            throw new Exception(string.Format("UnImplemented '{0}'", msIL));
+
+                        if (!item.SystemStack)
+                            throw new Exception(string.Format("UnImplemented-RegisterType '{0}'", msIL));
+
+                        new Pop { DestinationReg = Register.EAX };
+                        new Movsx { DestinationReg = Register.EAX, SourceReg = Register.AL, Size = 8 };
+                        new Push { DestinationReg = Register.EAX };
+
+                        Optimizer.vStack.Push(new StackItem(typeof(int)));
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+        }
+    }
+}
---
+++ src/Compiler/Atomixilc/IL/Conversion/Conv_U2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.IL.CodeType;
+using Atomixilc.Machine.x86;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Conv_U2)]
+    internal class Conv_U2_il : MSIL
+    {
+        internal Conv_U2_il()
+            : base(ILCode.Conv_U2)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Conv_U2(v=vs.110).aspx
+         * Description : Converts the value on top of the evaluation stack to native uint
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 2)
+                throw new Exception("Internal Compiler Error: vStack.Count < 2");
+
+            var item = Optimizer.vStack.Pop();
+            var size = Helper.GetTypeSize(item.OperandType, Config.TargetPlatform);
+
+            /* The stack transitional behavior, in sequential order, is:
+             * value is pushed onto the stack.
+             * value is popped from the stack and the conversion operation is attempted.
+             * If the conversion is successful, the resulting value is pushed onto the stack.
+             */
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        if (item.IsFloat || size > 4)
+                            throw new Exception(string.Format("UnImplemented '{0}'", msIL));
+
+                        if (!item.SystemStack)
+                            throw new Exception(string.Format("UnImplemented-RegisterType '{0}'", msIL));
+
+                        new Pop { DestinationReg = Register.EAX };
+                        new Movzx { DestinationReg = Register.EAX, SourceReg = Register.AX, Size = 16 };
+                        new Push { DestinationReg = Register.EAX };
+
+                        Optimizer.vStack.Push(new StackItem(typeof(uint)));
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+        }
+    }
+}
