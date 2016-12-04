0b717c8f8151c81d2e2305e64eb73dee0d34c207
Aman Priyadarshi <aman.eureka@outlook.com>
2016-12-04 11:18:41 +0530

    Added Ldc_il and Ldelem_il

---
+++ src/Compiler/Atomixilc/IL/Load/Ldc/Ldc_I4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.Machine.x86;
+using Atomixilc.IL.CodeType;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Ldc_I4)]
+    internal class Ldc_I4_il : MSIL
+    {
+        internal Ldc_I4_il()
+            : base(ILCode.Ldc_I4)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Ldc_I4(v=vs.110).aspx
+         * Description : Pushes a supplied value of type int32 onto the evaluation stack as an int32.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            var num = ((OpInt)xOp).Value;
+
+            /* The stack transitional behavior, in sequential order, is:
+             * The value num is pushed onto the stack
+             */
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        new Push { DestinationRef = "0x" + num.ToString("X") };
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+
+            Optimizer.vStack.Push(new StackItem(typeof(int)));
+        }
+    }
+}
---
+++ src/Compiler/Atomixilc/IL/Load/Ldelem/Ldelem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.Machine.x86;
+using Atomixilc.IL.CodeType;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Ldelem)]
+    internal class Ldelem_il : MSIL
+    {
+        internal Ldelem_il()
+            : base(ILCode.Ldelem)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Ldelem(v=vs.110).aspx
+         * Description : Loads the element at a specified array index onto the top of the evaluation stack as the type specified in the instruction.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 2)
+                throw new Exception("Internal Compiler Error: vStack.Count < 2");
+
+            var operand = ((OpType)xOp).Value;
+            var size = Helper.GetTypeSize(operand, Config.TargetPlatform);
+
+            /* The stack transitional behavior, in sequential order, is:
+             * An object reference array is pushed onto the stack.
+             * An index value index is pushed onto the stack.
+             * index and array are popped from the stack; the value stored at position index in array is looked up.
+             * The value is pushed onto the stack.
+             */
+
+            Optimizer.vStack.Pop();
+            Optimizer.vStack.Pop();
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        if (size > 4)
+                            throw new Exception(string.Format("UnImplemented '{0}'", msIL));
+
+                        Executex86(size, true);
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+
+            Optimizer.vStack.Push(new StackItem(operand));
+        }
+
+        internal static void Executex86(int size, bool IsSigned)
+        {
+            new Pop { DestinationReg = Register.EAX };
+            new Mov { DestinationReg = Register.EDX, SourceRef = "0x" + size.ToString("X") };
+            new Mul { DestinationReg = Register.EDX };
+
+            new Pop { DestinationReg = Register.EDX };
+            new Add { DestinationReg = Register.EDX, SourceReg = Register.EAX };
+
+            if (IsSigned)
+                new Movsx { DestinationReg = Register.EAX, SourceReg = Register.EDX, SourceIndirect = true, Size = (byte)(size * 8) };
+            else
+                new Movzx { DestinationReg = Register.EAX, SourceReg = Register.EDX, SourceIndirect = true, Size = (byte)(size * 8) };
+            new Push { DestinationReg = Register.EAX };
+        }
+    }
+}
