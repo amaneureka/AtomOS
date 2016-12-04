31f88aaf119da3572968811c8920a6c1d34c0fd1
Aman Priyadarshi <aman.eureka@outlook.com>
2016-12-04 11:46:19 +0530

    Added Ldind_il

---
+++ src/Compiler/Atomixilc/IL/Load/Ldind/Ldind_I.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.Machine.x86;
+using Atomixilc.IL.CodeType;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Ldind_I)]
+    internal class Ldind_I_il : MSIL
+    {
+        internal Ldind_I_il()
+            : base(ILCode.Ldind_I)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Ldind_I(v=vs.110).aspx
+         * Description : Loads a value of type native int as a native int onto the evaluation stack indirectly.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 1)
+                throw new Exception("Internal Compiler Error: vStack.Count < 1");
+
+            /* The stack transitional behavior, in sequential order, is:
+             * An address is pushed onto the stack.
+             * The address is popped from the stack; the value located at the address is fetched.
+             * The fetched value is pushed onto the stack.
+             */
+
+            Optimizer.vStack.Pop();
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        Executex86(4, true);
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+
+            Optimizer.vStack.Push(new StackItem(typeof(int)));
+        }
+
+        internal static void Executex86(int size, bool IsSigned)
+        {
+            new Pop { DestinationReg = Register.EDX };
+
+            if (IsSigned)
+                new Movsx { DestinationReg = Register.EAX, SourceReg = Register.EDX, SourceIndirect = true, Size = (byte)(size * 8) };
+            else
+                new Movzx { DestinationReg = Register.EAX, SourceReg = Register.EDX, SourceIndirect = true, Size = (byte)(size * 8) };
+            new Push { DestinationReg = Register.EAX };
+        }
+    }
+}
---
+++ src/Compiler/Atomixilc/IL/Load/Ldind/Ldind_Ref.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.Machine.x86;
+using Atomixilc.IL.CodeType;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Ldind_Ref)]
+    internal class Ldind_Ref_il : MSIL
+    {
+        internal Ldind_Ref_il()
+            : base(ILCode.Ldind_Ref)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Ldind_Ref(v=vs.110).aspx
+         * Description : Loads a value of type int32 as an int32 onto the evaluation stack indirectly.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 1)
+                throw new Exception("Internal Compiler Error: vStack.Count < 1");
+
+            /* The stack transitional behavior, in sequential order, is:
+             * An address is pushed onto the stack.
+             * The address is popped from the stack; the value located at the address is fetched.
+             * The fetched value is pushed onto the stack.
+             */
+
+            Optimizer.vStack.Pop();
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        Ldind_I_il.Executex86(4, true);
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+
+            Optimizer.vStack.Push(new StackItem(typeof(int)));
+        }
+    }
+}
