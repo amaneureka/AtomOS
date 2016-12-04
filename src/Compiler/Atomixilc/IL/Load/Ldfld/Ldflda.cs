39c6a8eae29041e16925a3ac2ebcc057344f2ecb
Aman Priyadarshi <aman.eureka@outlook.com>
2016-12-04 17:53:09 +0530

    Added Ldarg_il, Ldargs_il, Ldfld_il, Ldflda_il Added Shl, Shr x86 Instructions

---
+++ src/Compiler/Atomixilc/IL/Load/Ldfld/Ldflda.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.Machine.x86;
+using Atomixilc.IL.CodeType;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Ldflda)]
+    internal class Ldflda_il : MSIL
+    {
+        internal Ldflda_il()
+            : base(ILCode.Ldflda)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Ldflda(v=vs.110).aspx
+         * Description : Finds the address of a field in the object whose reference is currently on the evaluation stack.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 1)
+                throw new Exception("Internal Compiler Error: vStack.Count < 1");
+
+            var field = ((OpField)xOp).Value;
+            var offset = Helper.GetFieldOffset(Config, field.DeclaringType, field);
+
+            /* The stack transitional behavior, in sequential order, is:
+             * An object reference (or pointer) is pushed onto the stack.
+             * The object reference (or pointer) is popped from the stack; the address of the specified field in the object is found.
+             * The address of the specified field is pushed onto the stack.
+             */
+
+            Optimizer.vStack.Pop();
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        new Add { DestinationReg = Register.ESP, DestinationIndirect = true, SourceRef = "0x" + offset.ToString("X") };
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+
+            Optimizer.vStack.Push(new StackItem(typeof(uint)));
+        }
+    }
+}
---
+++ src/Compiler/Atomixilc/Machine/x86/Shl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Atomixilc.Machine.x86
+{
+    public class Shl : DestinationSourceSize
+    {
+        public Shl()
+            : base("shl")
+        {
+
+        }
+    }
+}
