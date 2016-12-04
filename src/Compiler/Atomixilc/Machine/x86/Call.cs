9181d371265e68e3b06ff0ffc114485f26431129
Aman Priyadarshi <aman.eureka@outlook.com>
2016-12-04 10:28:43 +0530

    Call_il and Ret_il Call x86 Instruction

---
+++ src/Compiler/Atomixilc/Machine/x86/Call.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Atomixilc.Machine.x86
+{
+    public class Call : Instruction
+    {
+        public string DestinationRef;
+
+        public Call()
+            : base("call")
+        {
+
+        }
+
+        public override string ToString()
+        {
+            if (DestinationRef.StartsWith("."))
+                DestinationRef = Label.Primary + DestinationRef;
+
+            return string.Format("call {0}", DestinationRef);
+        }
+    }
+}
