5a294da193494c61a82f0d0da96ee740aafd22df
Aman Priyadarshi <aman.eureka@outlook.com>
2016-12-08 19:42:08 +0530

    Implemented Ldstr_il Flush String Table Fixes!

--- src/Compiler/Atomixilc/IL/Load/Ldloc/Ldloc.cs
+++ src/Compiler/Atomixilc/IL/Load/Ldloc/Ldloc.cs
@@ -45,43 +45,7 @@ namespace Atomixilc.IL
                         if (size > 4)
                             throw new Exception("LocalVariable size > 4 not supported");
 
-                        switch(size)
-                        {
-                            case 1:
-                            case 2:
-                                {
-                                    if (Helper.IsSigned(varType))
-                                    {
-                                        new Movsx
-                                        {
-                                            DestinationReg = Register.EAX,
-                                            SourceReg = Register.EBP,
-                                            SourceIndirect = true,
-                                            SourceDisplacement = -EBPoffset,
-                                            Size = (byte)(size * 8)
-                                        };
-                                    }
-                                    else
-                                    {
-                                        new Movzx
-                                        {
-                                            DestinationReg = Register.EAX,
-                                            SourceReg = Register.EBP,
-                                            SourceIndirect = true,
-                                            SourceDisplacement = -EBPoffset,
-                                            Size = (byte)(size * 8)
-                                        };
-                                    }
-
-                                    new Push { DestinationReg = Register.EAX };
-                                }
-                                break;
-                            case 4:
-                                {
-                                    new Push { DestinationReg = Register.EBP, DestinationIndirect = true, DestinationDisplacement = -EBPoffset };
-                                }
-                                break;
-                        }
+                        new Push { DestinationReg = Register.EBP, DestinationIndirect = true, DestinationDisplacement = -EBPoffset };
                     }
                     break;
                 default:
---
+++ src/Compiler/Atomixilc/IL/Load/Ldstr.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.Machine.x86;
+using Atomixilc.IL.CodeType;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Ldstr)]
+    internal class Ldstr_il : MSIL
+    {
+        public Ldstr_il()
+            : base(ILCode.Ldstr)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Ldstr(v=vs.110).aspx
+         * Description : Pushes a new object reference to a string literal stored in the metadata.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            var str = ((OpString)xOp).Value;
+
+            /* The stack transitional behavior, in sequential order, is:
+             * An object reference to a string is pushed onto the stack.
+             */
+
+            new Comment(string.Format("[{0}] : {1} => {2}", ToString(), xOp.ToString(), Optimizer.vStack.Count));
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        new Push { DestinationRef = Helper.GetResolvedStringLabel(str) };
+
+                        Optimizer.vStack.Push(new StackItem(typeof(string)));
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+        }
+    }
+}
