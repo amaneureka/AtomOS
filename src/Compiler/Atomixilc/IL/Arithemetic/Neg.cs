74965b5c80323181084649830ffd6a6a3e18fab2
Aman Priyadarshi <aman.eureka@outlook.com>
2016-12-10 22:12:56 +0530

    Implemented Neg_il, Shl_il, Bne_Un_il

---
+++ src/Compiler/Atomixilc/IL/Arithemetic/Neg.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.Machine.x86;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Neg)]
+    internal class Neg_il : MSIL
+    {
+        public Neg_il()
+            : base(ILCode.Neg)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Neg(v=vs.110).aspx
+         * Description : Negates a value and pushes the result onto the evaluation stack.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 1)
+                throw new Exception("Internal Compiler Error: vStack.Count < 1");
+
+            var item = Optimizer.vStack.Pop();
+
+            var size = Helper.GetTypeSize(item.OperandType, Config.TargetPlatform);
+
+            /* The stack transitional behavior, in sequential order, is:
+             * A value is pushed onto the stack.
+             * A value is popped from the stack and negated.
+             * The result is pushed onto the stack.
+             */
+
+            new Comment(string.Format("[{0}] : {1} => {2}", ToString(), xOp.ToString(), Optimizer.vStack.Count));
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        if (item.IsFloat || size > 4)
+                            throw new Exception(string.Format("UnImplemented '{0}'", msIL));
+
+                        if (!item.SystemStack)
+                            throw new Exception(string.Format("UnImplemented-RegisterType '{0}'", msIL));
+
+                        new Pop { DestinationReg = Register.EAX };
+                        new Xor { DestinationReg = Register.EAX, SourceRef = "0xFFFFFFFF" };
+                        new Add { DestinationReg = Register.EAX, SourceRef = "0x1" };
+                        new Push { DestinationReg = Register.EAX };
+
+                        Optimizer.vStack.Push(new StackItem(item.OperandType));
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+        }
+    }
+}
---
+++ src/Compiler/Atomixilc/IL/Branch/Bne_Un.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.IL.CodeType;
+using Atomixilc.Machine.x86;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Bne_Un)]
+    internal class Bne_Un_il : MSIL
+    {
+        public Bne_Un_il()
+            : base(ILCode.Bne_Un)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Bne_Un(v=vs.110).aspx
+         * Description : Transfers control to a target instruction when two unsigned integer values or unordered float values are not equal.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 2)
+                throw new Exception("Internal Compiler Error: vStack.Count < 2");
+
+            var offset = ((OpBranch)xOp).Value;
+            var itemA = Optimizer.vStack.Pop();
+            var itemB = Optimizer.vStack.Pop();
+
+            var xTrueLabel = Helper.GetLabel(offset);
+
+            var size = Math.Max(Helper.GetTypeSize(itemA.OperandType, Config.TargetPlatform),
+                Helper.GetTypeSize(itemB.OperandType, Config.TargetPlatform));
+
+            /* The stack transitional behavior, in sequential order, is:
+             * value1 is pushed onto the stack.
+             * value2 is pushed onto the stack.
+             * value2 and value1 are popped from the stack; if value1 is not equal to value2, the branch operation is performed.
+             */
+
+            new Comment(string.Format("[{0}] : {1} => {2}", ToString(), xOp.ToString(), Optimizer.vStack.Count));
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        if (itemA.IsFloat || itemB.IsFloat || size > 4)
+                            throw new Exception(string.Format("UnImplemented '{0}'", msIL));
+
+                        if (!itemA.SystemStack || !itemB.SystemStack)
+                            throw new Exception(string.Format("UnImplemented-RegisterType '{0}'", msIL));
+
+                        new Pop { DestinationReg = Register.EDX };
+                        new Pop { DestinationReg = Register.EAX };
+                        new Cmp { DestinationReg = Register.EAX, SourceReg = Register.EDX };
+                        new Jmp { Condition = ConditionalJump.JNE, DestinationRef = xTrueLabel };
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+        }
+    }
+}
