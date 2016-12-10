2e1351827ae6de9eb90f76fc947271bf7d379039
Aman Priyadarshi <aman.eureka@outlook.com>
2016-12-11 01:07:57 +0530

    Implemented Shr, Shr_Un

---
+++ src/Compiler/Atomixilc/IL/Arithemetic/Shr.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Atomixilc.Machine;
+using Atomixilc.Attributes;
+using Atomixilc.Machine.x86;
+
+namespace Atomixilc.IL
+{
+    [ILImpl(ILCode.Shr)]
+    internal class Shr_il : MSIL
+    {
+        public Shr_il()
+            : base(ILCode.Shr)
+        {
+
+        }
+
+        /*
+         * URL : https://msdn.microsoft.com/en-us/library/system.reflection.emit.opcodes.Shr(v=vs.110).aspx
+         * Description : Shifts an integer value (in sign) to the right by a specified number of bits, pushing the result onto the evaluation stack.
+         */
+        internal override void Execute(Options Config, OpCodeType xOp, MethodBase method, Optimizer Optimizer)
+        {
+            if (Optimizer.vStack.Count < 2)
+                throw new Exception("Internal Compiler Error: vStack.Count < 2");
+
+            var itemA = Optimizer.vStack.Pop();
+            var itemB = Optimizer.vStack.Pop();
+
+            var size = Math.Max(Helper.GetTypeSize(itemA.OperandType, Config.TargetPlatform),
+                Helper.GetTypeSize(itemB.OperandType, Config.TargetPlatform));
+
+            /* The stack transitional behavior, in sequential order, is:
+             * A value is pushed onto the stack.
+             * The amount of bits to be shifted is pushed onto the stack.
+             * The number of bits to be shifted and the value are popped from the stack; the value is shifted right by the specified number of bits.
+             * The result is pushed onto the stack.
+             */
+
+            new Comment(string.Format("[{0}] : {1} => {2}", ToString(), xOp.ToString(), Optimizer.vStack.Count));
+
+            switch (Config.TargetPlatform)
+            {
+                case Architecture.x86:
+                    {
+                        if (itemA.IsFloat || itemB.IsFloat || size > 4)
+                            throw new Exception(string.Format("UnImplemented '{0}'", msIL));
+
+                        if (!itemA.SystemStack || !itemB.SystemStack)
+                            throw new Exception(string.Format("UnImplemented-RegisterType '{0}'", msIL));
+
+                        new Pop { DestinationReg = Register.ECX };
+                        new Sar { DestinationReg = Register.ESP, SourceReg = Register.CL, DestinationIndirect = true };
+
+                        Optimizer.vStack.Push(new StackItem(itemB.OperandType));
+                    }
+                    break;
+                default:
+                    throw new Exception(string.Format("Unsupported target platform '{0}' for MSIL '{1}'", Config.TargetPlatform, msIL));
+            }
+        }
+    }
+}
